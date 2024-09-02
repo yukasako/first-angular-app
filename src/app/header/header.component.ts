8755bfb669e6869b6bb115dbafdf0a0a759b80d8
yukasako <yukako.sakozono@yh.nackademin.se>
2024-09-02 14:10:22 +0200

    Initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header', //HTML内のelementの名前 
+    standalone: true,
+    templateUrl: `./header.component.html`, //HTML参照先
+    styleUrl: `./header.component.css`,
+  })
+
+export class HeaderComponent {}
