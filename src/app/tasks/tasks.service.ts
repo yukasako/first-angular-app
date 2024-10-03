8a1f73563b6cdd6132504ada2afe0f4a1881eb2c
yukasako <yukako.sakozono@yh.nackademin.se>
2024-10-03 18:05:12 +0200

    By lesson 63

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -34,6 +34,14 @@ export class TasksService {
     },
   ];
 
+  // LocalStorage（バックエンドからの内容と仮定）にある場合は、それをTaskオブジェトクとして使う。
+  constructor() {
+    const tasks = localStorage.getItem(`tasks`);
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -46,9 +54,15 @@ export class TasksService {
       summary: newTaskData.summary,
       dueDate: newTaskData.date,
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
