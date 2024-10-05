feb76af17664943e7e1824824120dd75cd576a47
yukasako <yukako.sakozono@yh.nackademin.se>
2024-10-06 00:31:43 +0200

    By section 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { InputComponent } from './tasks/inputTask/input/input.component';
-import { TaskComponent } from './tasks/task/task.component';
 
 @NgModule({
   // declarations: アプリで使うコンポーネント（AppComponent）を宣言。
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-    TasksComponent,
-    InputComponent,
-    TaskComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
   //   bootstrap: アプリの最初に表示するコンポーネントを指定（AppComponentが画面に表示される）。
   bootstrap: [AppComponent],
   // Standaloneをどうしても使いたい時は、DeclarationではなくImportで書く。
-  imports: [BrowserModule, FormsModule, SharedModule],
+  imports: [BrowserModule, SharedModule, TasksModule],
 })
 
 // AppModuleはアプリケーションのメインモジュール（ルートモジュール）を定義している。
--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { TasksComponent } from './tasks.component';
+import { InputComponent } from './inputTask/input/input.component';
+import { SharedModule } from '../shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  exports: [],
+  declarations: [TasksComponent, TaskComponent, InputComponent],
+  exports: [TasksComponent],
+  imports: [SharedModule, CommonModule, FormsModule],
 })
 export class TasksModule {}
